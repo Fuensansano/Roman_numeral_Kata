afa1c613e2dc17fcd268e2e47381b2bbf054ad0a
FuenSansano <i.fuensanosano@gmail.com>
2021-04-27 21:13:42 +0200

    GREEN: create the logic for 4

--- src/main/java/DecimalToRoman.java
+++ src/main/java/DecimalToRoman.java
@@ -1,10 +1,13 @@
 public class DecimalToRoman {
 
     public String convert(int number) {
-        String romanNumber = "";
-        for (int i = 0; i < number; i++){
-            romanNumber += "I";
+        if (number < 4){
+            String romanNumber = "";
+            for (int i = 0; i < number; i++){
+                romanNumber += "I";
+            }
+            return romanNumber;
         }
-        return romanNumber;
+        return "IV";
     }
 }
