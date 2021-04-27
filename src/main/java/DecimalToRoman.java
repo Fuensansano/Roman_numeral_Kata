57b9fd3fd1c3df83f775797728b85ab4c75f4cd1
FuenSansano <i.fuensanosano@gmail.com>
2021-04-27 21:05:07 +0200

    REFACTOR: remove if statements

--- src/main/java/DecimalToRoman.java
+++ src/main/java/DecimalToRoman.java
@@ -1,11 +1,10 @@
 public class DecimalToRoman {
 
     public String convert(int number) {
-        if (number == 3){
-            return "III";
-        } else if (number ==2) {
-            return "II";
+        String romanNumber = "";
+        for (int i = 0; i < number; i++){
+            romanNumber += "I";
         }
-        return "I";
+        return romanNumber;
     }
 }
