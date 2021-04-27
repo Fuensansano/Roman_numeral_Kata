3378a55dc036e58ea0ba88ba01b6c2e19672e1cd
FuenSansano <i.fuensanosano@gmail.com>
2021-04-27 21:46:44 +0200

    REFACTOR: refactoring the test to re-use the converter

--- src/test/java/DecimalToRomanTest.java
+++ src/test/java/DecimalToRomanTest.java
@@ -1,49 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 
 class DecimalToRomanTest {
 
+    private DecimalToRoman converter;
+
+    @BeforeEach
+    private void initEach() {
+        converter = new DecimalToRoman();
+    }
+
 	@Test
 	public void should_create_a_decimal_to_roman() {
-		DecimalToRoman decimalToRoman = new DecimalToRoman();
-		assertThat(decimalToRoman, instanceOf(DecimalToRoman.class));
+		assertThat(converter, instanceOf(DecimalToRoman.class));
 	}
 
 	@Test
     public void should_convert_1_to_I() {
-        DecimalToRoman decimalToRoman = new DecimalToRoman();
-        assertEquals(decimalToRoman.convert(1), "I");
+        assertEquals(converter.convert(1), "I");
     }
 
     @Test
     public void should_convert_2_to_II() {
-        DecimalToRoman decimalToRoman = new DecimalToRoman();
-        assertEquals(decimalToRoman.convert(2), "II");
+        assertEquals(converter.convert(2), "II");
     }
 
     @Test
     public void should_convert_3_to_III() {
-        DecimalToRoman decimalToRoman = new DecimalToRoman();
-        assertEquals(decimalToRoman.convert(3), "III");
+        assertEquals(converter.convert(3), "III");
     }
 
     @Test
     public void should_convert_4_to_IV() {
-        DecimalToRoman decimalToRoman = new DecimalToRoman();
-        assertEquals(decimalToRoman.convert(4), "IV");
+        assertEquals(converter.convert(4), "IV");
     }
 
     @Test
     public void should_convert_5_to_V(){
-	    DecimalToRoman decimalToRoman = new DecimalToRoman();
-	    assertEquals(decimalToRoman.convert(5),"V");
+	    assertEquals(converter.convert(5),"V");
     }
 
     @Test
     public void should_convert_6_to_VI(){
-	    DecimalToRoman decimalToRoman = new DecimalToRoman();
-	    assertEquals(decimalToRoman.convert(6),"VI");
+	    assertEquals(converter.convert(6),"VI");
     }
 }
