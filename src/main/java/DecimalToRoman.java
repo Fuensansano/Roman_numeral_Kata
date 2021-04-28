03ce7a2c13c74689fa43ef0bca074676175c48ae
FuenSansano <i.fuensanosano@gmail.com>
2021-04-28 15:16:18 +0200

    RED: Logic  900

--- src/main/java/DecimalToRoman.java
+++ src/main/java/DecimalToRoman.java
@@ -1,6 +1,6 @@
 public class DecimalToRoman {
-    private String symbols [] = {"D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-    private int values [] =     {500,400, 100, 90, 50,  40,  10,  9,  5,   4,  1};
+    private String symbols [] = {"CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    private int values [] =     {900, 500,400, 100, 90, 50,  40,  10,  9,  5,   4,  1};
     public String convert(int number) {
         String romanNumber = "";
 
